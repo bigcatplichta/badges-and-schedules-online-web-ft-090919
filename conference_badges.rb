ec722abe82028290860719c8f6c5656249d58a02
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 17:47:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(attendees)
   room_list
 end
 
-def printer(attendees)
+def printer
   batch_badge_creator(attendees).each_index { | i | puts attendees(i) }
   assign_rooms(attendees).each_index { | i | puts attendees(i) }
 end 
